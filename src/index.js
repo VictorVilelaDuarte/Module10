ac296f71bd0aa7db03b3ff60f0641c3f5405770f
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-12-09 01:08:06 -0300

    Configuração das rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import {View} from 'react-native';
-
+import Routes from './routes';
 // import { Container } from './styles';
 
 export default function src() {
-  return <View />;
+  return <Routes />;
 }
