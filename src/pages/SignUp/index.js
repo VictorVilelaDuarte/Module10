c197a0bfe26f3c459114a195c9515115cc4bf40d
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-12-09 01:28:34 -0300

    Configurando background padrão

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import {View} from 'react-native';
-
+import {Text} from 'react-native';
+import Background from '~/components/Background';
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <View />;
+  return (
+    <Background>
+      <Text>SignIn funcionando que é uma belezaaaaaaa</Text>
+    </Background>
+  );
 }
