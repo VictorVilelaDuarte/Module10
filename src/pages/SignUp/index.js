2dde7ee3b6c388dcd23eaa8b28c870350cc0f13b
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-12-11 00:09:42 -0300

    Configuração do Reactotron e Redux. Chamada a apis de SignIn e SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,9 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import {Image} from 'react-native';
-import Background from '~/components/Background';
+import {useDispatch, useSelector} from 'react-redux';
 
+import {signUpRequest} from '~/store/modules/auth/actions';
+import Background from '~/components/Background';
 import logo from '~/assets/logo.png';
 import {
   Container,
@@ -13,10 +15,18 @@ import {
 } from './styles';
 
 export default function SignUp({navigation}) {
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const emailRef = useRef();
   const passwordRef = useRef();
 
-  function handleSubmit() {}
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit() {
+    dispatch(signUpRequest(name, email, password));
+  }
 
   return (
     <Background>
@@ -30,6 +40,8 @@ export default function SignUp({navigation}) {
             placeholder="Digite seu nome"
             returnKeyType="next"
             onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
           />
 
           <FormInput
@@ -41,6 +53,8 @@ export default function SignUp({navigation}) {
             ref={emailRef}
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
 
           <FormInput
@@ -50,9 +64,13 @@ export default function SignUp({navigation}) {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Criar conta
+          </SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignIn')}>
